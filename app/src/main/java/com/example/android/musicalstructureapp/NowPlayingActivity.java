3bf25d84c72a9b554086557f64264ca71627c890
Shoxtrem <stanislas97@live.fr>
2018-06-13 09:38:42 +0200

    Added action bar to NowPlaying activity Added Labelling to activities

--- app/src/main/java/com/example/android/musicalstructureapp/NowPlayingActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/NowPlayingActivity.java
@@ -6,6 +6,8 @@ import java.lang.String;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class NowPlayingActivity extends AppCompatActivity {
@@ -33,6 +35,76 @@ public class NowPlayingActivity extends AppCompatActivity {
             TextView album = (TextView) findViewById(R.id.album_text_view);
             album.setText(songAlbum);
         }
+
+        // Find the view that shows the back button and add clickListener to it
+        ImageButton back = (ImageButton) findViewById(R.id.back_button);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        // Find the view that shows the songs button and add clickListener to it
+        ImageButton songs = (ImageButton) findViewById(R.id.song_button);
+
+        songs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NowPlayingActivity.this, SongsActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        // Find the view that shows the menu button and add clickListener to it
+        ImageButton menu = (ImageButton) findViewById(R.id.menu_button);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NowPlayingActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+        // Find the view that shows the album button and add clickListener to it
+        ImageButton album = (ImageButton) findViewById(R.id.album_button);
+
+        album.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NowPlayingActivity.this, AlbumActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        // Find the view that shows the artist button and add clickListener to it
+        ImageButton artist = (ImageButton) findViewById(R.id.artist_button);
+
+        artist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NowPlayingActivity.this, ArtistActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        // Find the view that shows the playlist button and add clickListener to it
+        ImageButton playlist = (ImageButton) findViewById(R.id.playlist_button);
+
+        playlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NowPlayingActivity.this, PlaylistActivity.class);
+
+                startActivity(intent);
+            }
+        });
 
     }
 
